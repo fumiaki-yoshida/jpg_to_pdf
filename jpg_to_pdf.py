5582247436d8619159fff650cad5f469906412a0
yoshida_fumiaki_F_132 <yoshida@glovic.jp>
2022-01-11 10:58:01 +0900

    fix: Fleak8に従ってきれいにした。

--- jpg_to_pdf.py
+++ jpg_to_pdf.py
@@ -1,29 +1,23 @@
-from PIL import Image
-import sys
-import cv2
-import numpy as np
-import pytesseract
 import os
 from glob import glob
-from tqdm import tqdm
-import PyPDF2
-from pdfminer.high_level import extract_text
-import re
-import shutil
 
-from reportlab.platypus import BaseDocTemplate, PageTemplate
-from reportlab.platypus import Paragraph, PageBreak, FrameBreak
-from reportlab.platypus.flowables import Spacer
-from reportlab.lib.styles import ParagraphStyle
+import PyPDF2
+import pytesseract
+from pdfminer.high_level import extract_text
 from reportlab.lib.pagesizes import A4, mm
+from reportlab.lib.styles import ParagraphStyle
 from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase import cidfonts
-from reportlab.platypus.frames import Frame
 from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import BaseDocTemplate, PageTemplate, Paragraph
+from reportlab.platypus.flowables import Spacer
+from reportlab.platypus.frames import Frame
+from tqdm import tqdm
+
 
 def imgfile_to_pdf(img_file):
     # gray = cv2.cvtColor(img_file, cv2.COLOR_BGR2GRAY)
-    pdf = pytesseract.image_to_pdf_or_hocr(img_file,
+    pdf = pytesseract.image_to_pdf_or_hocr(
+                                    img_file,
                                     lang="jpn",
                                     config="--dpi 300",
                                     extension='pdf'
@@ -31,17 +25,17 @@ def imgfile_to_pdf(img_file):
     root, ext = os.path.splitext(img_file)
     pdf_name = root + ".pdf"
     with open(pdf_name, 'w+b') as f:
-        f.write(pdf) # pdf type is bytes by default
+        f.write(pdf)  # pdf type is bytes by default
     return pdf_name
+
 
 def get_jpg_files(folder):
     jpg_files = glob(folder+"/*.jpg")
     return jpg_files
 
 
-def make_text_pdf(folder,text_list):
+def make_text_pdf(folder, text_list):
     file_path = folder+"/text.pdf"
-    
     pdfmetrics.registerFont(TTFont("yumin", "C:/Windows/Fonts/yuminl.ttf"))
     doc = BaseDocTemplate(
         file_path,
@@ -54,22 +48,23 @@ def make_text_pdf(folder,text_list):
     page_template = PageTemplate("frames", frames=frames)
     doc.addPageTemplates(page_template)
 
-    style_dict ={
-        "name":"normal",
-        "fontName":"yumin",
-        "fontSize":10,
-        "leading":10,
-        "firstLineIndent":10,
+    style_dict = {
+        "name": "normal",
+        "fontName": "yumin",
+        "fontSize": 10,
+        "leading": 10,
+        "firstLineIndent": 10,
         }
     style = ParagraphStyle(**style_dict)
 
     flowables = []
     space = Spacer(10*mm, 10*mm)
     for text in text_list:
-        para = Paragraph(text,style)
+        para = Paragraph(text, style)
         flowables.append(para)
     doc.multiBuild(flowables)
     return file_path
+
 
 folder = input("input scaned_file folder: ")
 jpg_files = get_jpg_files(folder)
@@ -80,10 +75,10 @@ for jpg_file in tqdm(jpg_files):
     pdf_name = imgfile_to_pdf(jpg_file)
     merger.append(pdf_name)
     pdfs.append(pdf_name)
-    text = extract_text(pdf_name).replace(" ","")
+    text = extract_text(pdf_name).replace(" ", "")
     text_list.append(text)
 
-text_pdf = make_text_pdf(folder=folder, text_list=text_list) 
+text_pdf = make_text_pdf(folder=folder, text_list=text_list)
 pdfs.append(text_pdf)
 merger.append(text_pdf)
 merged_name = extract_text(pdfs[0])
@@ -98,4 +93,3 @@ else:
     merger.close()
     for pdf_file in pdfs:
         os.remove(pdf_file)
-
